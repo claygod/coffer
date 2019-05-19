d6f1fe34b5cd04fd43cabb3bb81abb06483ab590
claygod <claygod@yandex.ru>
2019-05-19 11:17:18 +0300

    Add load method to checkpoint struct.

--- usecases/checkpoint.go
+++ usecases/checkpoint.go
@@ -6,15 +6,28 @@ package usecases
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"sort"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/claygod/coffer/domain"
 )
 
 type checkpoint struct {
-	dirPath string
+	dirPath    string
+	logger     Logger
+	resControl Resourcer
+}
+
+func newCheckpoint(path string, log Logger, rc Resourcer) *checkpoint {
+	return &checkpoint{
+		dirPath:    path,
+		logger:     log,
+		resControl: rc,
+	}
 }
 
 func (c *checkpoint) save(repo domain.RecordsRepository, fileName string) error {
@@ -50,15 +63,116 @@ func (c *checkpoint) save(repo domain.RecordsRepository, fileName string) error 
 
 }
 
-func (c *checkpoint) load(repo domain.RecordsRepository, fileName string) {
+func (c *checkpoint) load(repo domain.RecordsRepository, fileName string) error {
+	lastCheckoutName, err := c.getLastCheckPointName()
+	if err != nil {
+		return err
+	}
 
+	fl, err := os.Open(c.dirPath + lastCheckoutName)
+	if err != nil {
+		return err
+	}
+	stat, _ := fl.Stat()
+
+	if !c.resControl.GetPermission(stat.Size()) {
+		return fmt.Errorf("RecourcesControl: not permission for load operation. File size: %d", stat.Size())
+	}
+	return c.loadRecordsFromCheckpoint(fl, repo)
+}
+
+func (c *checkpoint) loadRecordsFromCheckpoint(f *os.File, repo domain.RecordsRepository) error {
+	rSize := make([]byte, 8)
+
+	//f.Seek(0, 0) //  whence: 0 начало файла, 1 текущее положение, and 2 от конца файла.
+	//var m runtime.MemStats
+	//runtime.ReadMemStats(&m)
+
+	for {
+		_, err := f.Read(rSize)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		rSuint64 := bytesToUint64(rSize)
+		sizeKey := int16(rSuint64)
+		sizeValue := rSuint64 >> 16
+
+		key := make([]byte, sizeKey)
+		n, err := f.Read(key)
+		if err != nil {
+			// if err == io.EOF { // тут EOF не должно быть?
+			// 	break
+			// }
+			return err
+		} else if n != int(sizeKey) {
+			return fmt.Errorf("The key is not fully loaded (%v)", key)
+		}
+
+		value := make([]byte, int(sizeValue))
+		n, err = f.Read(value)
+		if err != nil {
+			// if err == io.EOF { // тут EOF не должно быть?
+			// 	break
+			// }
+			return err
+		} else if n != int(sizeValue) {
+			return fmt.Errorf("The value is not fully loaded, (%v)", value)
+		}
+		rec := &domain.Record{
+			Key:   string(key),
+			Value: value,
+		}
+		repo.SetUnsafeRecord(rec)
+		//repo. (string(key), value)
+	}
+	return nil
+}
+
+func (c *checkpoint) getLastCheckPointName() (string, error) {
+	chpList, err := c.loadSuffixFilesList(".checkpoint")
+	if err != nil || len(chpList) == 0 {
+		return "", err
+	}
+	sort.Strings(chpList)
+	return chpList[len(chpList)-1], nil
+}
+
+func (c *checkpoint) loadSuffixFilesList(suffix string) ([]string, error) {
+	filesList, err := c.loadAllFilesList()
+	if err != nil {
+		return nil, err
+	}
+
+	suffList := make([]string, 0, len(filesList))
+	for _, fileName := range filesList {
+		if strings.HasSuffix(fileName, suffix) {
+			suffList = append(suffList, fileName)
+		}
+	}
+	return suffList, nil
+}
+
+func (c *checkpoint) loadAllFilesList() ([]string, error) {
+	fl, err := os.Open(c.dirPath)
+	if err != nil {
+		return nil, err
+	}
+	defer fl.Close()
+
+	return fl.Readdirnames(-1)
 }
 
 func (c *checkpoint) getNewCheckPointName() string {
 	for {
 		newFileName := c.dirPath + strconv.Itoa(int(time.Now().Unix())) + ".check"
-		if _, err := os.Stat(newFileName); !os.IsExist(err) {
+		_, err := os.Stat(newFileName)
+		if !os.IsExist(err) {
 			return newFileName
+		} else if err != nil {
+			c.logger.Write(err)
 		}
 		time.Sleep(1 * time.Second)
 	}
