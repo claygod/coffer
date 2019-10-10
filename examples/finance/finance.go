8f0a763709ad7ab9928b38710494c2ea7713458b
claygod <claygod@yandex.ru>
2019-10-10 21:18:04 +0300

    Finance example: debit, credit.

--- examples/finance/finance.go
+++ examples/finance/finance.go
@@ -5,10 +5,12 @@ package main
 // Copyright © 2019 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 
-	// "github.com/claygod/coffer"
-	// "github.com/claygod/coffer/domain"
+	"github.com/claygod/coffer"
+	"github.com/claygod/coffer/domain"
 	"github.com/claygod/coffer/examples"
 )
 
@@ -19,11 +21,77 @@ func main() {
 	defer examples.ClearDir(curDir)
 	fmt.Println("Financial example: BEGIN")
 
-	//TODO: add example
-	// Credit/debit
-	// Transfer
-	// Purchase/Sale
-	// Exchange
+	// STEP init
+	//hdlNewAccount := domain.Handler(HandlerNewAccount)
+	hdlCredit := domain.Handler(HandlerCredit)
+	hdlDebit := domain.Handler(HandlerDebit)
+	hdlTransfer := domain.Handler(HandlerTransfer)
+	//hdlMultiTransfer := domain.Handler(HandlerMultiTransfer)
+	db, err, wrn := coffer.Db(curDir).
+		//Handler("new_account", &hdlNewAccount).
+		Handler("credit", &hdlCredit).
+		Handler("debit", &hdlDebit).
+		Handler("transfer", &hdlTransfer).
+		//Handler("multi_transfer", &hdlMultiTransfer).
+		Create()
+	switch {
+	case err != nil:
+		fmt.Println("Error:", err)
+		return
+	case wrn != nil:
+		fmt.Println("Warning:", err)
+		return
+	case !db.Start():
+		fmt.Println("Error: not start")
+		return
+	}
+	defer db.Stop()
+	fmt.Println("- init: db created and started")
+
+	//STEP create accounts
+	if rep := db.WriteList(map[string][]byte{"john_usd": uint64ToBytes(100), "alice_usd": uint64ToBytes(5), "john_stock": uint64ToBytes(5), "alice_stock": uint64ToBytes(25)}); rep.IsCodeWarning() {
+		fmt.Printf("Write error: code `%d` msg `%s`", rep.Code, rep.Error)
+		return
+	}
+	fmt.Println("- accounts: created")
+
+	//STEP credit
+	if rep := db.Transaction("credit", []string{"john_usd"}, uint64(5)); rep.IsCodeWarning() {
+		fmt.Printf("Transaction error: code `%v` msg `%v`", rep.Code, rep.Error)
+		return
+	}
+	fmt.Println("- credit: 5 usd were withdrawn from John’s account")
+
+	//STEP debit
+	if rep := db.Transaction("debit", []string{"alice_stock"}, uint64(2)); rep.IsCodeWarning() {
+		fmt.Printf("Transaction error: code `%v` msg `%v`", rep.Code, rep.Error)
+		return
+	}
+	fmt.Println("- debit: two stocks added to Alice's account")
+
+	//STEP transfer
+	req1 := ReqTransfer{From: "john_usd", To: "alice_usd", Amount: 3}
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	if err := enc.Encode(req1); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if rep := db.Transaction("transfer", []string{"john_usd", "alice_usd"}, buf.Bytes()); rep.IsCodeWarning() {
+		fmt.Printf("Transaction error: code `%v` msg `%v`", rep.Code, rep.Error)
+		return
+	}
+	fmt.Println("- transfer: John transferred Alice $ 3")
+
+	//STEP purchase/sale
+	// t1 := &ReqTransfer{From: "john_usd", To: "alice_usd", Amount: 50}
+	// t2 := &ReqTransfer{From: "alice_stock", To: "john_stock", Amount: 5}
+	// req2 := []*ReqTransfer{t1, t2}
+	// if rep := db.Transaction("multi_transfer", []string{"john_usd", "alice_usd", "john_stock", "alice_stock"}, req2); rep.IsCodeWarning() {
+	// 	fmt.Printf("Transaction error: code `%v` msg `%v`", rep.Code, rep.Error)
+	// 	return
+	// }
+	// fmt.Println("- purchase/sale: John bought 5 stocks from Alice for $ 50")
 
 	fmt.Println("Financial example: END")
 }
--- examples/finance/handlers.go
+++ examples/finance/handlers.go
@@ -4,8 +4,176 @@ package main
 // Examples: finance handlers
 // Copyright © 2019 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"unsafe"
+)
+
+//TODO: ~~new account~~
 //TODO: add example
 //TODO: Credit/debit
 //TODO: Transfer
 //TODO: Purchase/Sale
-//TODO: Exchange
+//TODO: ~~Total~~
+//TODO: Exchange (use MultiTransfer)
+//TODO: ~~del account~~
+
+// func HandlerNewAccount(arg interface{}, recs map[string][]byte) (map[string][]byte, error) {
+// 	newAcc, ok := arg.(uint64)
+// 	if !ok {
+// 		return nil, fmt.Errorf("Invalid Argument: %v.", arg)
+// 	} else if len(recs) != 1 {
+// 		return nil, fmt.Errorf("Want 1 record, have %d", len(recs))
+// 	}
+// 	var recKey string
+// 	for k, _ := range recs {
+// 		recKey = k
+// 	}
+// 	return map[string][]byte{recKey: uint64ToBytes(newAcc)}, nil
+// }
+
+func HandlerCredit(arg interface{}, recs map[string][]byte) (map[string][]byte, error) {
+	delta, ok := arg.(uint64)
+	if !ok {
+		return nil, fmt.Errorf("Invalid Argument: %v.", arg)
+	} else if len(recs) != 1 {
+		return nil, fmt.Errorf("Want 1 record, have %d", len(recs))
+	}
+	var recKey string
+	var recValue []byte
+	for k, v := range recs {
+		recKey = k
+		recValue = v
+	}
+	if len(recValue) != 8 {
+		return nil, fmt.Errorf("The length of the value in the record is %d bytes, but 8 bytes are needed", len(recValue))
+	}
+	curAcc := bytesToUint64(recValue)
+	if delta > curAcc {
+		return nil, fmt.Errorf("Not enough funds in the account. There is %d, a credit of %d.", curAcc, delta)
+	}
+	return map[string][]byte{recKey: uint64ToBytes(curAcc - delta)}, nil
+}
+
+func HandlerDebit(arg interface{}, recs map[string][]byte) (map[string][]byte, error) {
+	delta, ok := arg.(uint64)
+	if !ok {
+		return nil, fmt.Errorf("Invalid Argument: %v.", arg)
+	} else if len(recs) != 1 {
+		return nil, fmt.Errorf("Want 1 record, have %d", len(recs))
+	}
+	var recKey string
+	var recValue []byte
+	for k, v := range recs {
+		recKey = k
+		recValue = v
+	}
+	if len(recValue) != 8 {
+		return nil, fmt.Errorf("The length of the value in the record is %d bytes, but 8 bytes are needed", len(recValue))
+	}
+	curAmount := bytesToUint64(recValue)
+	newAmount := curAmount + delta
+	if curAmount > newAmount {
+		return nil, fmt.Errorf("Account overflow. There is %d, a debit of %d.", curAmount, delta)
+	}
+	return map[string][]byte{recKey: uint64ToBytes(newAmount)}, nil
+}
+
+func HandlerTransfer(arg interface{}, recs map[string][]byte) (map[string][]byte, error) {
+	reqBytes, ok := arg.([]byte)
+	if !ok {
+		return nil, fmt.Errorf("Invalid Argument: %v.", arg)
+	}
+
+	dec := gob.NewDecoder(bytes.NewBuffer(reqBytes))
+	var req ReqTransfer
+	if err := dec.Decode(&req); err != nil {
+		return nil, err
+	}
+	fmt.Println("!!!", req)
+	// return &req, err
+
+	// req, ok := arg.(*ReqTransfer)
+	// if !ok {
+	// 	return nil, fmt.Errorf("Invalid Argument: %v.", arg)
+	// } else
+	if len(recs) != 2 {
+		return nil, fmt.Errorf("Want 2 record, have %d", len(recs))
+	}
+
+	recFromValueBytes, ok := recs[req.From]
+	if !ok {
+		return nil, fmt.Errorf("Entry %s cannot be found among transaction arguments.", req.From)
+	} else if len(recFromValueBytes) != 8 {
+		return nil, fmt.Errorf("The length of the value in the record `%s` is %d bytes, but 8 bytes are needed", req.From, len(recFromValueBytes))
+	}
+	recToValueBytes, ok := recs[req.To]
+	if !ok {
+		return nil, fmt.Errorf("Entry %s cannot be found among transaction arguments.", req.To)
+	} else if len(recFromValueBytes) != 8 {
+		return nil, fmt.Errorf("The length of the value in the record `%s` is %d bytes, but 8 bytes are needed", req.To, len(recToValueBytes))
+	}
+
+	recFromValueUint64 := bytesToUint64(recFromValueBytes)
+	newAmountFrom := recFromValueUint64 - req.Amount
+	if req.Amount > recFromValueUint64 {
+		return nil, fmt.Errorf("Not enough funds in the account `%s`. There is %d, a credit of %d.", req.From, recFromValueUint64, req.Amount)
+	}
+	recToValueUint64 := bytesToUint64(recToValueBytes)
+	newAmountTo := recToValueUint64 + req.Amount
+	if recToValueUint64 > newAmountTo {
+		return nil, fmt.Errorf("Account `%s` is overflow. There is %d, a debit of %d.", req.To, recToValueUint64, req.Amount)
+	}
+
+	return map[string][]byte{req.From: uint64ToBytes(newAmountFrom), req.To: uint64ToBytes(newAmountTo)}, nil
+}
+
+// func HandlerMultiTransfer(arg interface{}, recs map[string][]byte) (map[string][]byte, error) {
+// 	reqs, ok := arg.([]*ReqTransfer)
+// 	if !ok {
+// 		return nil, fmt.Errorf("Invalid Argument: %v.", arg)
+// 	} else if len(recs) != len(reqs)*2 {
+// 		return nil, fmt.Errorf("Want %d record, have %d", len(reqs)*2, len(recs))
+// 	}
+
+// 	out := make(map[string][]byte, len(recs))
+// 	for _, req := range reqs {
+// 		vFrom, ok := recs[req.From]
+// 		if !ok {
+// 			return nil, fmt.Errorf("Entry %s cannot be found among transaction arguments.", req.From)
+// 		}
+// 		vTo, ok := recs[req.From]
+// 		if !ok {
+// 			return nil, fmt.Errorf("Entry %s cannot be found among transaction arguments.", req.To)
+// 		}
+// 		m, err := HandlerTransfer(req, map[string][]byte{req.From: vFrom, req.To: vTo})
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		for k, v := range m {
+// 			out[k] = v
+// 		}
+// 	}
+// 	return out, nil
+// }
+
+type ReqTransfer struct {
+	From   string
+	To     string
+	Amount uint64
+}
+
+func uint64ToBytes(i uint64) []byte {
+	x := (*[8]byte)(unsafe.Pointer(&i))
+	out := make([]byte, 0, 8)
+	out = append(out, x[:]...)
+	return out
+}
+
+func bytesToUint64(b []byte) uint64 {
+	var x [8]byte
+	copy(x[:], b[:])
+	return *(*uint64)(unsafe.Pointer(&x))
+}
